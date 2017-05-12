ec22833af0f444c5ad90379fd10d22cdeb50f24b
Taras Ignashchenko <tignashchenko@gmail.com>
2017-05-12 10:44:01 -0700

    implement stringify function

--- jsoninator.js
+++ jsoninator.js
@@ -7,7 +7,31 @@ const _ = require('underscore'); // the real one! :)
 
 
 const stringify = function(obj) {
-  
+  let result = [];
+  let start, end;
+
+  if(typeof(obj) === 'number') {
+    return obj.toString();
+  } else if(obj === null) {
+    return '' + obj;
+  } else if(typeof(obj) === 'boolean') {
+    return '' + obj;
+  } else if(typeof(obj) === 'string') {
+    return '\"' + obj + '\"';
+  } else if(_.isArray(obj)) {
+    _.each(obj, function(element) {
+      result.push(stringify(element));
+    });
+    start = '[';
+    end = ']';
+  } else if(_.isObject(obj)) {
+    _.each(obj, function(value, key) {
+      result.push(stringify(key) + ':' + stringify(value));
+    });
+    start = '{';
+    end = '}';
+  }
+  return start + result + end;
 };
 
 module.exports = {
